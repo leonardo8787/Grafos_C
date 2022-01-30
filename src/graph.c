ec1ed878ec4ddb5c014959e5b5be3de3720388a8
leonardo8787 <leocampos8787@gmail.com>
2022-01-30 16:00:30 -0300

    atualizaçao

--- src/graph.c
+++ src/graph.c
@@ -45,3 +45,31 @@ void ImprimeGraph(Graph G) {
         printf("\n");
     }
 }
+
+// BFS algorithm
+/*
+void bfs(struct Graph* graph, int startVertex) {
+  struct queue* q = createQueue();
+
+  graph->visited[startVertex] = 1;
+  enqueue(q, startVertex);
+
+  while (!isEmpty(q)) {
+    printQueue(q);
+    int currentVertex = dequeue(q);
+    printf("Visited %d\n", currentVertex);
+
+    struct node* temp = graph->adjLists[currentVertex];
+
+    while (temp) {
+      int adjVertex = temp->vertex;
+
+      if (graph->visited[adjVertex] == 0) {
+        graph->visited[adjVertex] = 1;
+        enqueue(q, adjVertex);
+      }
+      temp = temp->next;
+    }
+  }
+}
+*/
