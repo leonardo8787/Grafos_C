24e8cd8d8ce7f0999456b17d3ae12af128212bfe
leonardo8787 <leocampos8787@gmail.com>
2022-01-30 16:40:45 -0300

    atualizaçao

--- src/graph.c
+++ src/graph.c
@@ -73,3 +73,24 @@ void bfs(struct Graph* graph, int startVertex) {
   }
 }
 */
+
+
+// DFS algo
+/*
+void DFS(struct Graph* graph, int vertex) {
+  struct node* adjList = graph->adjLists[vertex];
+  struct node* temp = adjList;
+
+  graph->visited[vertex] = 1;
+  printf("Visited %d \n", vertex);
+
+  while (temp != NULL) {
+    int connectedVertex = temp->vertex;
+
+    if (graph->visited[connectedVertex] == 0) {
+      DFS(graph, connectedVertex);
+    }
+    temp = temp->next;
+  }
+}
+*/
